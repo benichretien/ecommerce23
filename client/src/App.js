2157295e8c1ffa46002f8ab1db5e26c1b711dde8
andre <2702927@collegelacite.ca>
2023-02-07 12:45:49 -0500

    Creation des routes pour nos 3 pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
  export default function App() {
   return (
-    <div>
-      <h1>App component</h1>
-    </div>
+    <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+       </Routes>
+    </BrowserRouter>
   );
 }
 
