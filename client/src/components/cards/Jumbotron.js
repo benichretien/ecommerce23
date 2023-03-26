f506f6d569dd23fd92f83bde56fb24f4db61301f
andre <2702927@collegelacite.ca>
2023-03-26 01:32:30 -0400

    display products

--- client/src/components/cards/Jumbotron.js
+++ client/src/components/cards/Jumbotron.js
@@ -2,9 +2,9 @@ import "../cards/jumbotron.css";
 import logo from "../cards/logo.png";
 
 export default function Jumbotron({title, subtitle="L'endroit prefere d'article animalier"}){
-    return <div className="container-fluid bg-couleur">
+    return <div className="container-fluid jumbotron">
         <div className="row">
-            <div className="col text-center p-5 bg-coral">
+            <div className="col text-center p-5">
                 <div className="milieu">
                   <h1>{title}</h1>
                   <p className="lead">{subtitle}</p>
--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,16 +1,44 @@
 import moment from "moment"
-
+import { Badge } from "antd"
 
 export default function ProductCart({p}) {
     return (
-        <div className="card mb-3">
-            <img src={`${process.env.REACT_APP_API}/product/photo/${p._id}`} 
-            alt={p.name}
-            style={{height: "300px", objectFit: "cover"}}
-            />
-           <p>{p.name}</p>
-           <p>{moment(p.createdAt).fromNow()}</p>
-           <p>{p.sold} vendu</p>
+        <div className="card mb-3 hoverable">
+            <Badge.Ribbon text={`${p?.sold} vendu`} color="red">
+                <Badge.Ribbon text={`${p?.quantity >= 1 ? `${p?.quantity - p?.sold} En stock` : "Hors stock"}`}
+                placement="start" color="green"
+                >
+                  <img src={`${process.env.REACT_APP_API}/product/photo/${p._id}`} 
+                  alt={p.name}
+                  className="card-img-top"
+                  style={{height: "300px", objectFit: "cover"}}/>
+                </Badge.Ribbon>
+            </Badge.Ribbon>
+
+            <div className="card-body">
+              <h5>{p?.name}</h5>
+
+              <h4 className="fw-bold">
+                {p?.price?.toLocaleString("en-US", {style: "currency", currency: "CAD",})}
+              </h4>
+
+
+
+              <p className="card-text">{p?.description?.substring(0, 60)}...</p>
+            </div>
+
+            <div className="d-flex justify-content-between">
+                <button className="btn btn-primary col card-button" style={{borderBottomLeftRadius: "5px"}}>
+                    Voir le produit
+                </button>
+
+                <button className="btn btn-outline-primary col card-button" style={{borderBottomRightRadius: "5px"}}>Ajouter au panier</button>
+            </div>
+
+
+
+           {/*<p>{moment(p.createdAt).fromNow()}</p>
+           <p>{p.sold} vendu</p>*/}
         </div>
     )
 }
